0081efd22f333a86228e569f5158ef3cc14abd22
Aiden Huynh <aidenhh009@gmail.com>
2026-06-16 13:56:57 -0700

    Add Python profile variables lab

---
+++ datatypes.py
@@ -0,0 +1,4 @@
+books = ["MazeRunner", "AGGGTM", "HungerGames", "EnderGames"]
+print (books)
+dictionary = {"animal": "dogs", "color": "yellow", "number": 9, "hobbie": "reading"}
+print (dictionary)
