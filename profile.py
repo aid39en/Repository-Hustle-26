58b976ee616a507b6c035ff8d17062506c18dc9d
Aiden Huynh <aidenhh009@gmail.com>
2026-06-16 13:51:25 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,12 @@
+full_name = "Aiden Huynh"
+nickname = "Aiden"
+age = 15
+used_python_before = "True (code.org)"
+hobbies = ["reading", "watching shows", "writing"] 
+favorites = { 
+ "book": "Project: Hail Mary", 
+ "hobby": "reading", 
+ "color": "yellow" 
+} 
+languages_spoken = {"English", "Cantonese", "Minimal-Spanish"} # set: no duplicate
+print(full_name, age, hobbies, used_python_before, favorites, languages_spoken) 
